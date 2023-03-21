401ff5aba91c22159980ccc14db3b9c3a6fcbd83
Ernesto Cordero <ernesto.cordero@pfcti.com>
2023-03-20 19:40:25 -0600

    Subiendo trabajo

--- Lab2/lab/lab/src/main/java/com/example/lab/DTO/ProductoDto.java
+++ Lab2/lab/lab/src/main/java/com/example/lab/DTO/ProductoDto.java
@@ -1,4 +1,21 @@
 package com.example.lab.DTO;
 
+import jakarta.persistence.Column;
+import lombok.Data;
+
+
+@Data
 public class ProductoDto {
+
+    private int id;
+
+    @Column(name = "nombre"
+            , length = 50
+            , columnDefinition = "varchar(50)")
+    private String nombre;
+
+    @Column(name = "estado"
+            , length = 50
+            , columnDefinition = "varchar(50)")
+    private String estado;
 }
