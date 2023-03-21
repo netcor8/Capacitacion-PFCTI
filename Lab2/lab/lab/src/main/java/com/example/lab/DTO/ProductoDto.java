37fd3bd9847ec9736abfc59f46a28041f8e1a6e0
Ernesto Cordero <ernesto.cordero@pfcti.com>
2023-03-20 19:40:59 -0600

    Subiendo

--- Lab2/lab/lab/src/main/java/com/example/lab/DTO/ProductoDto.java
+++ Lab2/lab/lab/src/main/java/com/example/lab/DTO/ProductoDto.java
@@ -9,13 +9,9 @@ public class ProductoDto {
 
     private int id;
 
-    @Column(name = "nombre"
-            , length = 50
-            , columnDefinition = "varchar(50)")
+
     private String nombre;
 
-    @Column(name = "estado"
-            , length = 50
-            , columnDefinition = "varchar(50)")
+
     private String estado;
 }
